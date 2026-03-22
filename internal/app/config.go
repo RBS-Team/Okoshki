1272094814286936814e04b310d48fc615214907
Romaniwe <safonov.300@yandex.ru>
2026-03-23 01:50:08 +0300

    Работаем над cors и csrf

--- internal/app/config.go
+++ internal/app/config.go
@@ -3,16 +3,18 @@ package app
 import (
 	"time"
 
+	"github.com/RBS-Team/Okoshki/internal/middleware"
 	"github.com/spf13/viper"
 )
 
 type HTTPConfig struct {
-	Port            string        `mapstructure:"port"`
-	ReadTimeout     time.Duration `mapstructure:"readTimeout"`
-	WriteTimeout    time.Duration `mapstructure:"writeTimeout"`
-	IdleTimeout     time.Duration `mapstructure:"idleTimeout"`
-	ShutdownTimeout time.Duration `mapstructure:"shutdownTimeout"`
-	Auth            AuthConfig    `mapstructure:"auth"`
+	Port            string                `mapstructure:"port"`
+	ReadTimeout     time.Duration         `mapstructure:"readTimeout"`
+	WriteTimeout    time.Duration         `mapstructure:"writeTimeout"`
+	IdleTimeout     time.Duration         `mapstructure:"idleTimeout"`
+	ShutdownTimeout time.Duration         `mapstructure:"shutdownTimeout"`
+	Auth            AuthConfig            `mapstructure:"auth"`
+	CORS            middleware.CORSConfig `mapstructure:"cors"`
 }
 
 type LoggerConfig struct {
@@ -22,6 +24,7 @@ type LoggerConfig struct {
 
 type AuthConfig struct {
 	JWT  JWTConfig  `mapstructure:"jwt"`
+	CSRF CSRFConfig `mapstructure:"csrf"`
 }
 
 type JWTConfig struct {
@@ -29,13 +32,20 @@ type JWTConfig struct {
 	AccessTokenTTL time.Duration `mapstructure:"accessTokenTTL"`
 }
 
+type CSRFConfig struct {
+	SecretKey string        `mapstructure:"secretKey"`
+	TokenTTL  time.Duration `mapstructure:"tokenTTL"`
+}
+
 func BindViperEnv(v *viper.Viper) error {
 	bindings := map[string]string{
-		"db.password":                  "DB_PASSWORD",
-		"db.user":                      "DB_USER",
-		"db.host":                      "DB_HOST",
-		"db.dbName":                    "DB_NAME",
-		"auth.http.auth.jwt.secretKey": "JWT_SECRET",
+		"db.password": "DB_PASSWORD",
+		"db.user":     "DB_USER",
+		"db.host":     "DB_HOST",
+		"db.dbName":   "DB_NAME",
+
+		"auth.http.auth.jwt.secretKey":  "JWT_SECRET",
+		"auth.http.auth.csrf.secretKey": "CSRF_SECRET",
 	}
 
 	for configKey, envKey := range bindings {
